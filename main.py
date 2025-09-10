1c83cda82fdb2caa8965c305702766c90ad2c3b8
LuSiEDa <wltlrtmdgus@naver.com>
2025-09-10 20:44:45 +0900

    커밋 너무 많아

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
 
 
 from contextlib import asynccontextmanager
+
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
 #     # Startup
