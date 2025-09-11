f39be807073839f61f8dd7db4842a78399dfb05a
LuSiEDa <wltlrtmdgus@naver.com>
2025-09-11 19:36:03 +0900

    t005

--- app/models/models.py
+++ app/models/models.py
@@ -7,7 +7,6 @@ class User(models.Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=255)
     email = fields.CharField(max_length=255, unique=True)
-    password = fields.CharField(max_length=255)
     password_hash = fields.CharField(max_length=255)
     nickname = fields.CharField(max_length=255, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
